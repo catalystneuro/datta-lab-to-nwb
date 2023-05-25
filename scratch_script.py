be35adc6d262c81696e3e0782548ef06ef4a4d4c
pauladkisson <paul.wesley.adkisson@gmail.com>
2023-05-25 15:39:52 -0700

    prototype behavioral interface

---
+++ scratch_script.py
@@ -0,0 +1,136 @@
+# scratch script to test various implementations in datta-lab-to-nwb
+from datta_lab_to_nwb.markowitz_gillis_nature_2023 import markowitz_gillis_nature_2023behaviorinterface
+from pynwb import NWBFile
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import colorcet as cc
+
+def run_conversion():
+    filename = "/Volumes/T7/CatalystNeuro/NWB/Datta/dopamine-reinforces-spontaneous-behavior/dlight_raw_data/dlight_photometry_processed_full.parquet"
+    interface = markowitz_gillis_nature_2023behaviorinterface.MarkowitzGillisNature2023BehaviorInterface(filename)
+    example_session = '2891f649-4fbd-4119-a807-b8ef507edfab'
+    metadata = interface.get_metadata(example_session)
+    metadata['reference_frame'] = "???" # TODO: add reference frame to metadata
+    print(f'metadata: {metadata}')
+    nwbfile = NWBFile(
+        session_description=metadata['session_name'],
+        identifier=metadata['uuid'],
+        session_start_time=metadata['date'],
+        experimenter=[
+            'Jeffrey E. Markowitz',
+            'Winthrop F. Gillis',
+            'Maya Jay',
+            'Jeffrey Wood',
+            'Ryley W. Harris',
+            'Robert Cieszkowski',
+            'Rebecca Scott',
+            'David Brann',
+            'Dorothy Koveal',
+            'Tomasz Kula',
+            'Caleb Weinreb',
+            'Mohammed Abdal Monium Osman',
+            'Sandra Romero Pinto',
+            'Naoshige Uchida',
+            'Scott W. Linderman',
+            'Bernardo L. Sabatini',
+            'Sandeep Robert Datta'
+        ],
+        lab="Datta Lab",
+        institution="Harvard Medical School",
+        experiment_description="TODO",
+        session_id=metadata['uuid'],
+    )
+    nwbfile = interface.run_conversion(nwbfile, metadata, example_session)
+    return nwbfile
+
+def reproduce_fig1d(nwbfile):
+    n_frames = 361
+    start = 3520
+    position = pd.DataFrame(nwbfile.processing['behavior']['Position']['SpatialSeries'].data,
+                            columns=['x', 'y', 'height'])
+    angle = pd.DataFrame(nwbfile.processing['behavior']['CompassDirection']['SpatialSeries'].data,
+                         columns=['angle'])
+    syllables = pd.DataFrame(nwbfile.processing['behavior']['SyllableTimeSeries'].time_series['syllable'].data,
+                             columns=['syllable'])
+    vel_height = (
+            position['height']
+            .interpolate(limit_direction="both")
+            .diff(2)
+            .iloc[start:start + n_frames]
+            .to_numpy()
+            / 2
+            * 30
+    )
+    velocity = np.sqrt(
+        position["x"].interpolate(limit_direction="both").diff() ** 2 +
+        position["y"].interpolate(limit_direction="both").diff() ** 2
+    )
+    acc = velocity.diff(2).iloc[start:start + n_frames].to_numpy() * 30
+    vel = velocity.iloc[start:start + n_frames].to_numpy() * 30
+    vel_angle = angle.interpolate(limit_direction="both").diff(2).iloc[start:start + n_frames].to_numpy() * 30 * -1
+    syllables = syllables.iloc[start:start + n_frames].to_numpy()
+    t = np.arange(n_frames) / 30
+
+    time = t
+    fig, ax = plt.subplots(6, 1)
+    fig.dpi = 200
+
+    ax[0].plot(time, vel, color='k')
+    ax[0].set_xlim(time[0], time[-1])
+    ax[0].set_xticks([time[0], time[-1]])
+    ax[0].set_xticklabels([])
+    ax[0].set_ylim(0, 15 * 30)
+    ax[0].set_yticks([0, 15 * 30])
+    ax[0].set_ylabel('mm/s')
+    ax[0].text(0, 1, "Velocity", transform=ax[0].transAxes)
+    ax[0].spines['top'].set_visible(False)
+    ax[0].spines['right'].set_visible(False)
+
+    ax[1].plot(time, acc, color='k')
+    ax[1].set_xlim(time[0], time[-1])
+    ax[1].set_xticks([time[0], time[-1]])
+    ax[1].set_xticklabels([])
+    ax[1].set_ylim(-5 * 30, 5 * 30)
+    ax[1].set_ylabel('mm/s2')
+    ax[1].set_yticks([-150, 0, 150])
+    ax[1].text(0, 1, "Acceleration", transform=ax[1].transAxes)
+    ax[1].spines['top'].set_visible(False)
+    ax[1].spines['right'].set_visible(False)
+
+    ax[2].plot(time, vel_angle, color='k')
+    ax[2].set_xlim(time[0], time[-1])
+    ax[2].set_xticks([time[0], time[-1]])
+    ax[2].set_xticklabels([])
+    ax[2].set_ylim(-0.55 * 30, 0.55 * 30)
+    ax[2].set_yticks([-16, 0, 16])
+    ax[2].set_ylabel('rad/s')
+    ax[2].text(0, 1, "Angular velocity", transform=ax[2].transAxes)
+    ax[2].spines['top'].set_visible(False)
+    ax[2].spines['right'].set_visible(False)
+
+    ax[3].plot(time, vel_height, color='k')
+    ax[3].set_xlim(time[0], time[-1])
+    ax[3].set_xticks([time[0], time[-1]])
+    ax[3].set_xticklabels([])
+    ax[3].set_ylim(-5 * 30, 5 * 30)
+    ax[3].set_yticks([-5 * 30, 0, 5 * 30])
+    ax[3].set_ylabel('mm/s')
+    ax[3].text(0, 1, "Height Velocity", transform=ax[3].transAxes)
+    ax[3].spines['top'].set_visible(False)
+    ax[3].spines['right'].set_visible(False)
+
+    ax[4].imshow(syllables[np.newaxis, :], aspect='auto', interpolation='none', cmap=cc.cm.glasbey, vmin=0)
+    ax[4].set_xticks([])
+    ax[4].set_yticks([])
+    ax[4].text(0, 1.1, "Syllable", transform=ax[4].transAxes)
+
+    plt.subplots_adjust(hspace=0.55)
+    plt.suptitle("Figure 1d")
+    plt.show()
+
+
+
+if __name__ == '__main__':
+    nwbfile = run_conversion()
+    reproduce_fig1d(nwbfile)
--- src/datta_lab_to_nwb/markowitz_gillis_nature_2023/markowitz_gillis_nature_2023behaviorinterface.py
+++ src/datta_lab_to_nwb/markowitz_gillis_nature_2023/markowitz_gillis_nature_2023behaviorinterface.py
@@ -1,22 +1,108 @@
 """Primary class for converting experiment-specific behavior."""
-from pynwb.file import NWBFile
-
+import numpy as np
+import pandas as pd
+from pynwb import NWBFile, TimeSeries
+from pynwb.behavior import (
+    BehavioralTimeSeries,
+    CompassDirection,
+    Position,
+    SpatialSeries,
+)
 from neuroconv.basedatainterface import BaseDataInterface
 
 class MarkowitzGillisNature2023BehaviorInterface(BaseDataInterface):
     """Behavior interface for markowitz_gillis_nature_2023 conversion"""
 
-    def __init__(self):
+    def __init__(self, filename: str):
         # This should load the data lazily and prepare variables you need
-        pass
+        columns = (
+            'predicted_syllable (offline)',
+            'uuid',
+            'date',
+            'session_name',
+            'SessionName',
+            'mouse_id',
+            'centroid_x_mm',
+            'centroid_y_mm',
+            'height_ave_mm',
+            'angle_unwrapped',
+            'timestamp',
+        )
+        metadata_columns = ["uuid", "date", "mouse_id"]
+        super().__init__(filename=filename, columns=columns, metadata_columns=metadata_columns)
 
-    def get_metadata(self):
-        # Automatically retrieve as much metadata as possible
+    def get_metadata(self, session_uuid: str) -> dict:
+        """Get metadata about the experiment as a whole."""
+        # TODO: store metadata in .yaml file
         metadata = super().get_metadata()
+
+        # get session metadata TODO: move session metadata to a separate file to avoid multiple reads
+        session_df = pd.read_parquet(self.source_data['filename'],
+                                     columns=self.source_data['columns'],
+                                     filters=[('uuid', '==', session_uuid)])
+        for col in self.source_data['metadata_columns']:
+            first_notnull = session_df.loc[session_df[col].notnull(), col].iloc[0]
+            metadata[col] = first_notnull
+        session_name = (
+                set(session_df.session_name[session_df.session_name.notnull()]) |
+                set(session_df.SessionName[session_df.SessionName.notnull()])
+                        )
+        assert len(session_name) == 1, "Multiple session names found"
+        metadata['session_name'] = session_name.pop()
         
         return metadata
 
-    def run_conversion(self, nwbfile: NWBFile, metadata: dict):
-        # All the custom code to write to PyNWB
-        
+
+    def run_conversion(self, nwbfile: NWBFile, metadata: dict, session_uuid: str) -> NWBFile:
+        """Run conversion of data from the source file into the nwbfile."""
+        session_df = pd.read_parquet(self.source_data['filename'],
+                                     columns=self.source_data['columns'],
+                                     filters=[('uuid', '==', session_uuid)])
+
+        # Add Position Data
+        position_data = np.vstack((session_df.centroid_x_mm, session_df.centroid_y_mm, session_df.height_ave_mm)).T
+        position_spatial_series = SpatialSeries(
+            name="SpatialSeries",
+            description="Position (x, y, height) in an open field.",
+            data=position_data,
+            timestamps=session_df.timestamp.to_numpy(),
+            reference_frame=metadata['reference_frame'],
+            unit="mm",
+        )
+        position = Position(spatial_series=position_spatial_series)
+
+        # Add Compass Direction Data
+        direction_spatial_series = SpatialSeries(
+            name="SpatialSeries",
+            description="View angle of the subject measured in radians.",
+            data=session_df.angle_unwrapped.to_numpy(),
+            timestamps=session_df.timestamp.to_numpy(),
+            reference_frame=metadata['reference_frame'],
+            unit="radians",
+        )
+        direction = CompassDirection(
+            spatial_series=direction_spatial_series, name="CompassDirection"
+        )
+
+        # Add Syllable Data
+        syllable_time_series = TimeSeries(
+            name="syllable",
+            data=session_df["predicted_syllable (offline)"].to_numpy(),
+            timestamps=session_df.timestamp.to_numpy(),
+            description="Behavioral Syllable identified by Motion Sequencing (MoSeq).",
+            unit="None",
+        )
+        behavioral_time_series = BehavioralTimeSeries(
+            time_series=syllable_time_series,
+            name="SyllableTimeSeries",
+        )
+
+        # Combine all data into a behavioral processing module
+        behavior_module = nwbfile.create_processing_module(
+            name="behavior", description="Processed behavioral data"
+        )
+        behavior_module.add(position)
+        behavior_module.add(direction)
+        behavior_module.add(behavioral_time_series)
+
         return nwbfile
