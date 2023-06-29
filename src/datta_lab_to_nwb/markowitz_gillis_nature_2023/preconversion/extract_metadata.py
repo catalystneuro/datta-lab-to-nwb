acff935bf326938358932d419614a95ea92b649e
pauladkisson <paul.wesley.adkisson@gmail.com>
2023-06-28 17:09:56 -0700

    refactored metadata to split by session vs. subject

--- src/datta_lab_to_nwb/markowitz_gillis_nature_2023/preconversion/extract_metadata.py
+++ src/datta_lab_to_nwb/markowitz_gillis_nature_2023/preconversion/extract_metadata.py
@@ -10,7 +10,10 @@ from neuroconv.utils import dict_deep_update
 
 
 def extract_photometry_metadata(
-    data_path: str, example_uuid: str = None, num_sessions: int = None, reinforcement_photometry: bool = False
+    data_path: str,
+    example_uuid: str = None,
+    num_sessions: int = None,
+    reinforcement_photometry: bool = False,
 ) -> dict:
     """Extract metadata from photometry data.
 
@@ -32,7 +35,7 @@ def extract_photometry_metadata(
         Dictionary of metadata.
     """
     photometry_data_path = Path(data_path) / "dlight_raw_data/dlight_photometry_processed_full.parquet"
-    columns = (
+    session_columns = (
         "uuid",
         "session_name",
         "SessionName",
@@ -42,16 +45,12 @@ def extract_photometry_metadata(
         "reference_max",
         "signal_reference_corr",
         "snr",
-        "area",
     )
-    metadata_columns = (
-        "date",
+    subject_columns = (
         "mouse_id",
-        "signal_max",
-        "reference_max",
-        "signal_reference_corr",
-        "snr",
+        "genotype",
         "area",
+        "opsin",
     )
     photometry_reinforcement_mouse_ids = {
         "dlight-chrimson-1",
@@ -68,25 +67,29 @@ def extract_photometry_metadata(
     else:
         filters = [("mouse_id", "not in", photometry_reinforcement_mouse_ids)]
     if example_uuid is None:
-        uuid_df = pd.read_parquet(
+        df = pd.read_parquet(
             photometry_data_path,
             columns=["uuid", "mouse_id"],
             filters=filters,
         )
-        uuids = set(uuid_df.uuid[uuid_df.uuid.notnull()])
-        del uuid_df
+        uuids = set(df.uuid[df.uuid.notnull()])
+        del df
     else:
         uuids = {example_uuid}
     if num_sessions is None:
         num_sessions = len(uuids)
-    metadata = {}
-    for i, uuid in enumerate(tqdm(uuids)):
-        extract_session_metadata(columns, photometry_data_path, metadata, uuid)
-        metadata[uuid]["photometry_area"] = metadata[uuid].pop("area")
-        if i >= num_sessions:
+    session_metadata = {}
+    for i, uuid in enumerate(tqdm(uuids, desc="Extracting photometry session metadata")):
+        extract_session_metadata(session_columns, photometry_data_path, session_metadata, uuid)
+        if i + 1 >= num_sessions:
             break
+    subject_ids = set(session_metadata[uuid]["subject_id"] for uuid in session_metadata)
+    subject_metadata = {}
+    for mouse_id in tqdm(subject_ids, desc="Extracting photometry subject metadata"):
+        extract_subject_metadata(subject_columns, photometry_data_path, subject_metadata, mouse_id)
+        subject_metadata[mouse_id]["photometry_area"] = subject_metadata[mouse_id].pop("area")
 
-    return metadata
+    return session_metadata, subject_metadata
 
 
 def extract_reinforcement_metadata(
@@ -112,57 +115,63 @@ def extract_reinforcement_metadata(
         Dictionary of metadata.
     """
     reinforcement_data_path = Path(data_path) / "optoda_raw_data/closed_loop_behavior.parquet"
-    columns = (
+    session_columns = (
         "uuid",
         "SessionName",
         "date",
-        "opsin",
-        "genotype",
-        "area",
         "mouse_id",
-        "sex",
         "exclude",
-        "cohort",
         "stim_duration",
         "stim_frequency",
         "pulse_width",
         "power",
+    )
+    subject_columns = (
+        "mouse_id",
+        "sex",
+        "opsin",
+        "genotype",
+        "area",
+        "cohort",
     )
     if reinforcement_photometry:
         filters = [("experiment_type", "==", "reinforcement_photometry")]
     else:
         filters = [("experiment_type", "==", "reinforcement")]
     if example_uuid is None:
-        uuid_df = pd.read_parquet(
+        df = pd.read_parquet(
             reinforcement_data_path,
-            columns=["uuid", "experiment_type"],
+            columns=["uuid", "experiment_type", "mouse_id"],
             filters=filters,
         )
-        uuids = set(uuid_df.uuid[uuid_df.uuid.notnull()])
-        del uuid_df
+        uuids = set(df.uuid[df.uuid.notnull()])
+        del df
     else:
         uuids = {example_uuid}
-    metadata = {}
+    session_metadata, subject_metadata = {}, {}
     if num_sessions is None:
         num_sessions = len(uuids)
-    for i, uuid in enumerate(tqdm(uuids)):
-        extract_session_metadata(columns, reinforcement_data_path, metadata, uuid)
-        metadata[uuid]["optogenetic_area"] = metadata[uuid].pop("area")
-        if metadata[uuid]["sex"] == "male":
-            metadata[uuid]["sex"] = "M"
-        elif metadata[uuid]["sex"] == "female":
-            metadata[uuid]["sex"] = "F"
-        else:
-            metadata[uuid]["sex"] = "U"
+    for i, uuid in enumerate(tqdm(uuids, desc="Extracting reinforcement session metadata")):
+        extract_session_metadata(session_columns, reinforcement_data_path, session_metadata, uuid)
         # add si units to names
-        metadata[uuid]["stim_duration_s"] = metadata[uuid].pop("stim_duration")
-        metadata[uuid]["stim_frequency_Hz"] = metadata[uuid].pop("stim_frequency")
-        metadata[uuid]["pulse_width_s"] = metadata[uuid].pop("pulse_width")
-        metadata[uuid]["power_watts"] = metadata[uuid].pop("power") / 1000
-        if i >= num_sessions:
+        session_metadata[uuid]["stim_duration_s"] = session_metadata[uuid].pop("stim_duration")
+        session_metadata[uuid]["stim_frequency_Hz"] = session_metadata[uuid].pop("stim_frequency")
+        session_metadata[uuid]["pulse_width_s"] = session_metadata[uuid].pop("pulse_width")
+        session_metadata[uuid]["power_watts"] = session_metadata[uuid].pop("power") / 1000
+        if i + 1 >= num_sessions:
             break
+    subject_ids = set(session_metadata[uuid]["subject_id"] for uuid in session_metadata)
+    for mouse_id in tqdm(subject_ids, desc="Extracting reinforcement subject metadata"):
+        extract_subject_metadata(subject_columns, reinforcement_data_path, subject_metadata, mouse_id)
+        subject_metadata[mouse_id]["optogenetic_area"] = subject_metadata[mouse_id].pop("area")
+        if subject_metadata[mouse_id]["sex"] == "male":
+            subject_metadata[mouse_id]["sex"] = "M"
+        elif subject_metadata[mouse_id]["sex"] == "female":
+            subject_metadata[mouse_id]["sex"] = "F"
+        else:
+            subject_metadata[mouse_id]["sex"] = "U"
 
-    return metadata
+    return session_metadata, subject_metadata
 
 
 def extract_reinforcement_photometry_metadata(
@@ -184,41 +193,57 @@ def extract_reinforcement_photometry_metadata(
     metadata : dict
         Dictionary of metadata.
     """
-    photometry_metadata = extract_photometry_metadata(
+    photometry_session_metadata, photometry_subject_metadata = extract_photometry_metadata(
         data_path, example_uuid, num_sessions, reinforcement_photometry=True
     )
-    reinforcement_metadata = extract_reinforcement_metadata(
+    reinforcement_session_metadata, reinforcement_subject_metadata = extract_reinforcement_metadata(
         data_path, example_uuid, num_sessions, reinforcement_photometry=True
     )
-    photometry_uuids = set(photometry_metadata.keys())
-    reinforcement_uuids = set(reinforcement_metadata.keys())
+    photometry_uuids = set(photometry_session_metadata.keys())
+    reinforcement_uuids = set(reinforcement_session_metadata.keys())
+    session_metadata = resolve_duplicates(
+        photometry_session_metadata, photometry_uuids, reinforcement_session_metadata, reinforcement_uuids
+    )
+    photometry_subject_ids = set(photometry_subject_metadata.keys())
+    reinforcement_subject_ids = set(reinforcement_subject_metadata.keys())
+    subject_metadata = resolve_duplicates(
+        photometry_subject_metadata, photometry_subject_ids, reinforcement_subject_metadata, reinforcement_subject_ids
+    )
+    return session_metadata, subject_metadata
+
+
+def resolve_duplicates(photometry_metadata, photometry_ids, reinforcement_metadata, reinforcement_ids):
     metadata = {}
-    for uuid in photometry_uuids:
-        for photometry_key in photometry_metadata[uuid].keys():
+    for photometry_id in photometry_ids:
+        for photometry_key in photometry_metadata[photometry_id].keys():
             try:
                 assert (
-                    photometry_metadata[uuid][photometry_key] == reinforcement_metadata[uuid][photometry_key]
-                ), f"photometry metadata and reinforcement metadata don't match (photometry[{uuid}][{photometry_key}]: {photometry_metadata[uuid][photometry_key]}, reinforcement[{uuid}][{photometry_key}]: {reinforcement_metadata[uuid][photometry_key]})"
-            except KeyError:  # reinforcement metadata doesn't have this uuid and/or metadata field
+                    photometry_metadata[photometry_id][photometry_key]
+                    == reinforcement_metadata[photometry_id][photometry_key]
+                ), f"photometry metadata and reinforcement metadata don't match (photometry[{photometry_id}][{photometry_key}]: {photometry_metadata[photometry_id][photometry_key]}, reinforcement[{photometry_id}][{photometry_key}]: {reinforcement_metadata[photometry_id][photometry_key]})"
+            except KeyError:  # reinforcement metadata doesn't have this id and/or metadata field
                 pass
             try:
-                metadata[uuid][photometry_key] = photometry_metadata[uuid][photometry_key]
-            except KeyError:  # New uuid
-                metadata[uuid] = {}
-                metadata[uuid][photometry_key] = photometry_metadata[uuid][photometry_key]
-    for uuid in reinforcement_uuids:
-        for reinforcement_key in reinforcement_metadata[uuid].keys():
+                metadata[photometry_id][photometry_key] = photometry_metadata[photometry_id][photometry_key]
+            except KeyError:  # New id
+                metadata[photometry_id] = {}
+                metadata[photometry_id][photometry_key] = photometry_metadata[photometry_id][photometry_key]
+    for reinforcement_id in reinforcement_ids:
+        for reinforcement_key in reinforcement_metadata[reinforcement_id].keys():
             try:
                 assert (
-                    photometry_metadata[uuid][reinforcement_key] == reinforcement_metadata[uuid][reinforcement_key]
-                ), f"photometry metadata and reinforcement metadata don't match (photometry[{uuid}][{reinforcement_key}]: {photometry_metadata[uuid][reinforcement_key]}, reinforcement[{uuid}][{reinforcement_key}]: {reinforcement_metadata[uuid][reinforcement_key]})"
+                    photometry_metadata[reinforcement_id][reinforcement_key]
+                    == reinforcement_metadata[reinforcement_id][reinforcement_key]
+                ), f"photometry metadata and reinforcement metadata don't match (photometry[{reinforcement_id}][{reinforcement_key}]: {photometry_metadata[reinforcement_id][reinforcement_key]}, reinforcement[{reinforcement_id}][{reinforcement_key}]: {reinforcement_metadata[reinforcement_id][reinforcement_key]})"
             except KeyError:
                 pass
             try:
-                metadata[uuid][reinforcement_key] = reinforcement_metadata[uuid][reinforcement_key]
+                metadata[photometry_id][reinforcement_key] = reinforcement_metadata[reinforcement_id][reinforcement_key]
             except KeyError:
-                metadata[uuid] = {}
-                metadata[uuid][reinforcement_key] = reinforcement_metadata[uuid][reinforcement_key]
+                metadata[reinforcement_id] = {}
+                metadata[reinforcement_id][reinforcement_key] = reinforcement_metadata[reinforcement_id][
+                    reinforcement_key
+                ]
     return metadata
 
 
@@ -237,29 +262,50 @@ def extract_session_metadata(columns, data_path, metadata, uuid):
         UUID of the session to extract metadata from.
     """
     timezone = pytz.timezone("America/New_York")
-    session_df = pd.read_parquet(
+    extract_metadata(columns, data_path, metadata, uuid, "uuid")
+    date = timezone.localize(metadata[uuid].pop("date"))
+    metadata[uuid]["session_start_time"] = date.isoformat()
+    metadata[uuid]["subject_id"] = metadata[uuid].pop("mouse_id")
+
+
+def extract_subject_metadata(columns, data_path, metadata, subject_id):
+    """Extract metadata from a single subject specified by subject_id.
+
+    Parameters
+    ----------
+    columns : tuple
+        Columns to load from the parquet file.
+    data_path : str
+        Path to the parquet file.
+    metadata : dict
+        Dictionary to store the metadata in (edited in-place).
+    subject_id : str
+        Subject ID of the subject to extract metadata from.
+    """
+    extract_metadata(columns, data_path, metadata, subject_id, "mouse_id")
+
+
+def extract_metadata(columns, data_path, metadata, key, key_name):
+    df = pd.read_parquet(
         data_path,
         columns=columns,
-        filters=[("uuid", "==", uuid)],
+        filters=[(key_name, "==", key)],
     )
-    metadata[uuid] = {}
+    metadata[key] = {}
     for col in columns:
-        if col in {"uuid", "session_name", "SessionName"}:
+        if col in {key_name, "session_name", "SessionName"}:
             continue
         try:
-            first_notnull = session_df.loc[session_df[col].notnull(), col].iloc[0]
+            first_notnull = df.loc[df[col].notnull(), col].iloc[0]
         except IndexError:  # No non-null values found
             first_notnull = np.NaN
         try:  # numpy arrays aren't serializable --> extract relevant value with .item()
             first_notnull = first_notnull.item()
         except AttributeError:
             pass
-        metadata[uuid][col] = first_notnull
+        metadata[key][col] = first_notnull
     if "session_name" in columns or "SessionName" in columns:
-        metadata[uuid]["session_description"] = get_session_name(session_df)
-    date = timezone.localize(metadata[uuid].pop("date"))
-    metadata[uuid]["session_start_time"] = date.isoformat()
-    metadata[uuid]["subject_id"] = metadata[uuid].pop("mouse_id")
+        metadata[key]["session_description"] = get_session_name(df)
 
 
 def get_session_name(session_df):
@@ -290,22 +336,32 @@ def get_session_name(session_df):
 
 if __name__ == "__main__":
     data_path = Path("/Volumes/T7/CatalystNeuro/NWB/Datta/dopamine-reinforces-spontaneous-behavior")
-    photometry_metadata_path = Path(
-        "/Volumes/T7/CatalystNeuro/NWB/Datta/dopamine-reinforces-spontaneous-behavior/metadata/photometry_metadata.yaml"
-    )
-    reinforcement_metadata_path = Path(
-        "/Volumes/T7/CatalystNeuro/NWB/Datta/dopamine-reinforces-spontaneous-behavior/metadata/reinforcement_metadata.yaml"
-    )
-    reinforcement_photometry_metadata_path = Path(
-        "/Volumes/T7/CatalystNeuro/NWB/Datta/dopamine-reinforces-spontaneous-behavior/metadata/reinforcement_photometry_metadata.yaml"
-    )
+    metadata_path = data_path / "metadata"
+    photometry_session_metadata_path = metadata_path / "photometry_session_metadata.yaml"
+    photometry_subject_metadata_path = metadata_path / "photometry_subject_metadata.yaml"
+    reinforcement_session_metadata_path = metadata_path / "reinforcement_session_metadata.yaml"
+    reinforcement_subject_metadata_path = metadata_path / "reinforcement_subject_metadata.yaml"
+    reinforcement_photometry_session_metadata_path = metadata_path / "reinforcement_photometry_session_metadata.yaml"
+    reinforcement_photometry_subject_metadata_path = metadata_path / "reinforcement_photometry_subject_metadata.yaml"
+
     example_uuid = "2891f649-4fbd-4119-a807-b8ef507edfab"
-    reinforcement_metadata = extract_reinforcement_metadata(data_path, num_sessions=3)
-    photometry_metadata = extract_photometry_metadata(data_path, num_sessions=3)
-    reinforcement_photometry_metadata = extract_reinforcement_photometry_metadata(data_path, example_uuid=example_uuid)
-    with open(photometry_metadata_path, "w") as f:
-        yaml.dump(photometry_metadata, f)
-    with open(reinforcement_metadata_path, "w") as f:
-        yaml.dump(reinforcement_metadata, f)
-    with open(reinforcement_photometry_metadata_path, "w") as f:
-        yaml.dump(reinforcement_photometry_metadata, f)
+    reinforcement_session_metadata, reinforcement_subject_metadata = extract_reinforcement_metadata(
+        data_path, num_sessions=3
+    )
+    photometry_session_metadata, photometry_subject_metadata = extract_photometry_metadata(data_path, num_sessions=3)
+    (
+        reinforcement_photometry_session_metadata,
+        reinforcement_photometry_subject_metadata,
+    ) = extract_reinforcement_photometry_metadata(data_path, example_uuid=example_uuid)
+    path2metadata = {
+        photometry_session_metadata_path: photometry_session_metadata,
+        photometry_subject_metadata_path: photometry_subject_metadata,
+        reinforcement_session_metadata_path: reinforcement_session_metadata,
+        reinforcement_subject_metadata_path: reinforcement_subject_metadata,
+        reinforcement_photometry_session_metadata_path: reinforcement_photometry_session_metadata,
+        reinforcement_photometry_subject_metadata_path: reinforcement_photometry_subject_metadata,
+    }
+
+    for path, metadata in path2metadata.items():
+        with open(path, "w") as f:
+            yaml.dump(metadata, f)
