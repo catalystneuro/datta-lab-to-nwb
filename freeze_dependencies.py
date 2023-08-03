c8018c66ec9843087bc2e5e369591070c79629bc
pauladkisson <paul.wesley.adkisson@gmail.com>
2023-08-03 12:57:12 -0700

    added dependency freezing to pre-commit hooks for providence

--- freeze_dependencies.py
+++ freeze_dependencies.py
@@ -1,9 +1,11 @@
 import subprocess
+import os
 
 
 def main():
-    with open("frozen_dependencies.txt", "w") as f:
-        subprocess.run(["pip", "freeze"], stdout=f)
+    # with open("frozen_dependencies.txt", "w") as f:
+    #     subprocess.run(["pip", "freeze"], stdout=f)
+    os.system("pip freeze > frozen_dependencies.txt")
 
 
 if __name__ == "__main__":
