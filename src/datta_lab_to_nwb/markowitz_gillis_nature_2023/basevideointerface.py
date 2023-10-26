fixed uint32 overflow bug for timestamps and refactored into utils.py module

src/datta_lab_to_nwb/markowitz_gillis_nature_2023/basevideointerface.py
@@ -7,6 +7,7 @@ import numpy as np
 import pandas as pd
 from neuroconv.datainterfaces import VideoInterface
 from .basedattainterface import BaseDattaInterface
+from .utils import convert_timestamps_to_seconds
 
 
 class BaseVideoInterface(BaseDattaInterface):
@@ -33,13 +34,11 @@ class BaseVideoInterface(BaseDattaInterface):
         )
 
     def get_original_timestamps(self) -> np.ndarray:
-        return pd.read_csv(self.source_data["timestamp_path"]).to_numpy().squeeze()
+        return pd.read_csv(self.source_data["timestamp_path"], header=None).to_numpy().squeeze()
 
     def align_timestamps(self, metadata: dict) -> np.ndarray:
         timestamps = self.get_original_timestamps()
-        TIMESTAMPS_TO_SECONDS = metadata["Constants"]["TIMESTAMPS_TO_SECONDS"]
-        timestamps -= timestamps[0]
-        timestamps = timestamps * TIMESTAMPS_TO_SECONDS
+        timestamps = convert_timestamps_to_seconds(timestamps=timestamps, metadata=metadata)
 
         self.set_aligned_timestamps(aligned_timestamps=timestamps)
         if self.source_data["alignment_path"] is not None:

src/datta_lab_to_nwb/markowitz_gillis_nature_2023/moseqextractinterface.py
@@ -6,6 +6,7 @@ import h5py
 import numpy as np
 from hdmf.backends.hdf5.h5_utils import H5DataIO
 from .basedattainterface import BaseDattaInterface
+from .utils import convert_timestamps_to_seconds
 from pynwb.image import GrayscaleImage, ImageMaskSeries
 from pynwb import TimeSeries
 from pynwb.behavior import (
@@ -45,9 +46,7 @@ class MoseqExtractInterface(BaseDattaInterface):
 
     def align_timestamps(self, metadata: dict) -> np.ndarray:
         timestamps = self.get_original_timestamps()
-        TIMESTAMPS_TO_SECONDS = metadata["Constants"]["TIMESTAMPS_TO_SECONDS"]
-        timestamps -= timestamps[0]
-        timestamps = timestamps * TIMESTAMPS_TO_SECONDS
+        timestamps = convert_timestamps_to_seconds(timestamps=timestamps, metadata=metadata)
 
         self.set_aligned_timestamps(aligned_timestamps=timestamps)
         if self.source_data["alignment_path"] is not None: