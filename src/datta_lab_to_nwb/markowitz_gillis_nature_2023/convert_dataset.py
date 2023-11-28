5232bb9eba94d0b98f212badd9bc30385bf5b094
pauladkisson <paul.wesley.adkisson@gmail.com>
2023-11-27 17:01:35 -0800

    fixed bugs with dataset conversion script

--- src/datta_lab_to_nwb/markowitz_gillis_nature_2023/convert_dataset.py
+++ src/datta_lab_to_nwb/markowitz_gillis_nature_2023/convert_dataset.py
@@ -10,16 +10,16 @@ def dataset_to_nwb(processed_path: Union[str, Path], raw_dir_path: Union[str, Pa
     output_dir_path.mkdir(parents=True, exist_ok=True)
 
     for experimental_folder in raw_dir_path.iterdir():
-        assert experimental_folder.is_dir(), f"{experimental_folder} is not a directory"
-        for session_folder in experimental_folder.iterdir():
-            assert session_folder.is_dir(), f"{session_folder} is not a directory"
-            results_file = session_folder / "proc" / "results00.yaml"
-            results = load_dict_from_file(results_file)
-            assert "uuid" in results, f"UUID not found in {results_file}"
+        if experimental_folder.is_dir():
+            for session_folder in experimental_folder.iterdir():
+                if session_folder.is_dir():
+                    results_file = session_folder / "proc" / "results_00.yaml"
+                    results = load_dict_from_file(results_file)
+                    assert "uuid" in results, f"UUID not found in {results_file}"
 
 
 if __name__ == "__main__":
-    processed_path = "/path/to/processed"
-    raw_dir_path = "/path/to/raw"
-    output_dir_path = "/path/to/output"
+    processed_path = Path("/Volumes/T7/CatalystNeuro/NWB/Datta/dopamine-reinforces-spontaneous-behavior")
+    raw_dir_path = Path("/Volumes/T7/CatalystNeuro/NWB/Datta/formatted_raw")
+    output_dir_path = Path("/Volumes/T7/CatalystNeuro/NWB/Datta/conversion_nwb/")
     dataset_to_nwb(processed_path, raw_dir_path, output_dir_path)
